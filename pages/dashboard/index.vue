36e5692aecb893f14d8834403363b419565cfddd
honeyfriolo <honey.friolo@lsu.edu.ph>
2021-11-24 17:52:44 +0800

    Pagination, Search, Add button

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -13,8 +13,7 @@
           rounded
           block
           uppercase
-          w-full
-        w-full
+          w-full w-full
         "
       >
         patients
@@ -31,7 +30,7 @@
           rounded
           block
           uppercase
-        w-full
+          w-full
         "
       >
         inventory
@@ -48,7 +47,7 @@
           rounded
           block
           uppercase
-        w-full
+          w-full
         "
       >
         settings
@@ -65,49 +64,148 @@
           rounded
           block
           uppercase
-        w-full
+          w-full
         "
       >
         logout
       </button>
     </div>
-    <div class="w-9/12 ">
-    <table class="">
-  <tr>
-    <th>ID</th>
-    <th>Patient's Name</th>
-    <th>Age</th>
-    <th>Telephone</th>
-    <th>Date</th>
-    <th>Date</th>
-    <th>Person 1</th>
-    <th>Person 2</th>
-    <th>Person 3</th>
-  </tr>
-  <tr>
-    <td>Emil</td>
-    <td>Tobias</td>
-    <td>Linus</td>
-  </tr>
-  <tr>
-    <td>16</td>
-    <td>14</td>
-    <td>10</td>
-  </tr>
-  <tr>
-    <td>16</td>
-    <td>14</td>
-    <td>10</td>
-  </tr>
-</table>
-    
-    
+
+    <div class="w-9/12 mx-auto text-center mt-20">
+      <div
+        class="relative flex w-80 h-9 float-right flex-wrap items-stretch mb-3"
+      >
+        <span
+          class="
+            z-10
+            h-full
+            leading-snug
+            font-normal
+            absolute
+            text-center text-gray-400
+            absolute
+            bg-transparent
+            rounded
+            text-base
+            items-center
+            justify-center
+            w-8
+            pl-2
+            py-1
+          "
+        >
+          <i class="fas fa-search"></i>
+        </span>
+        <input
+          type="text"
+          placeholder="Search"
+          class="
+            px-2
+            py-1
+            placeholder-gray-400
+            text-gray-600
+            relative
+            bg-white bg-white
+            rounded-full
+            text-sm
+            border border-gray-400
+            outline-none
+            focus:outline-none focus:ring
+            w-full
+            pl-10
+          "
+        />
+      </div>
+      <table class="border border-black w-full">
+        <thead>
+          <tr class="h-10 text-center border border-black">
+            <th class="w-20">ID</th>
+            <th>Patient's Name</th>
+            <th>Age</th>
+            <th>Telephone</th>
+            <th>Date</th>
+            <th>Address</th>
+            <th>Complain</th>
+            <th>Procedure</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="h-10">
+            <td>14</td>
+            <td>Honey Grace Friolo</td>
+            <td>18</td>
+            <td>09383069002</td>
+            <td>07/10/21</td>
+            <td>Aguada</td>
+            <td>Sensitivity</td>
+            <td>Extraction</td>
+          </tr>
+          <tr class="h-10">
+            <td>14</td>
+            <td>Honey Grace Friolo</td>
+            <td>18</td>
+            <td>09383069002</td>
+            <td>07/10/21</td>
+            <td>Aguada</td>
+            <td>Sensitivity</td>
+            <td>Extraction</td>
+          </tr>
+          <tr class="h-10">
+            <td>14</td>
+            <td>Honey Grace Friolo</td>
+            <td>18</td>
+            <td>09383069002</td>
+            <td>07/10/21</td>
+            <td>Aguada</td>
+            <td>Sensitivity</td>
+            <td>Extraction</td>
+          </tr>
+          <tr class="h-10">
+            <td>14</td>
+            <td>Honey Grace Friolo</td>
+            <td>18</td>
+            <td>09383069002</td>
+            <td>07/10/21</td>
+            <td>Aguada</td>
+            <td>Sensitivity</td>
+            <td>Extraction</td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+      <div>
+        <button
+          class="
+            mt-20
+            hover:text-blue-500
+             hover:border-blue-500
+             border
+             hover:bg-white
+            py-2
+            px-5
+            float-right
+            rounded-lg
+            font-bold
+            mt-5
+            text-white
+            ml-5
+            bg-blue-500
+            
+          "
+        >
+          Add Account
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Pagination from "../../components/Pagination.vue";
+export default {
+  components: { Pagination },
+};
 </script>
 
 <style>
